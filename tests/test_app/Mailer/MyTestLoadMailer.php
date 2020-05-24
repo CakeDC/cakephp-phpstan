01282fad6e28bb5ce7fee0a9e2818b2ffbd8a540
Marcelo Rocha <contato@omarcelo.com.br>
2020-05-24 14:38:09 -0300

    Added service for Cell::loadModel

---
+++ tests/test_app/Mailer/MyTestLoadMailer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
+ *  @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace App\Mailer;
+
+use Cake\Mailer\Mailer;
+
+class MyTestLoadMailer extends Mailer
+{
+
+    /**
+     * Test for TableLocatorDynamicReturnTypeExtension with loadModel
+     *
+     * @return void
+     */
+    protected function sampleLoading()
+    {
+        $article = $this->loadModel('VeryCustomize00009Articles')
+            ->newSample();
+        $this->viewBuilder()->setVar('article', $article);
+    }
+}
---
+++ tests/test_app/View/Cell/MyTestLoadCell.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
+ *  @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+namespace App\View\Cell;
+
+use Cake\View\Cell;
+
+class MyTestLoadCell extends Cell
+{
+    /**
+     * Test for TableLocatorDynamicReturnTypeExtension with loadModel
+     *
+     * @return void
+     */
+    public function sample()
+    {
+        $article = $this->loadModel('VeryCustomize00009Articles')
+            ->newSample();
+        $this->set('article', $article);
+    }
+}

17fbf007e7ed6eca052a86078ba5a1d5478fb9fb
Marcelo Rocha <contato@omarcelo.com.br>
2024-02-02 13:03:19 -0300

    Fixing issue with findBy methods

--- tests/test_app/Model/Table/VeryCustomize00009ArticlesTable.php
+++ tests/test_app/Model/Table/VeryCustomize00009ArticlesTable.php
@@ -42,8 +42,14 @@ class VeryCustomize00009ArticlesTable extends Table
     {
         //logic to clear article
         $this->fakeData();
+        /**
+         * @var \Cake\ORM\Entity $article
+         */
         $article = $this->findByTitle('sample')->first();
         $article->set('title', 'sample two');
+        /**
+         * @var \Cake\ORM\Entity $article
+         */
         $article = $this->findByTitleAndActive('sample', true)->first();
         $article->set('title', 'sample two');
 

f3058ca12f0fbf62523b7ec06784f5d34437a68c
Marcelo Rocha <contato@omarcelo.com.br>
2024-10-31 08:39:16 -0300

    PHPStan 2.0 Upgrade - Fix deprecated usage

--- src/Type/BaseTraitExpressionTypeResolverExtension.php
+++ src/Type/BaseTraitExpressionTypeResolverExtension.php
@@ -62,10 +62,10 @@ class BaseTraitExpressionTypeResolverExtension implements ExpressionTypeResolver
         }
 
         $callerType = $scope->getType($expr->var);
-        if (!$callerType instanceof ThisType && !$callerType instanceof ObjectType) {
+        if (!$callerType->isObject()->yes()) {
             return null;
         }
-        $reflection = $callerType->getClassReflection();
+        $reflection = $callerType->getObjectClassReflections()[0] ?? null;
         if ($reflection === null || !$this->isFromTargetTrait($reflection, $this->targetTrait)) {
             return null;
         }
@@ -96,16 +96,17 @@ class BaseTraitExpressionTypeResolverExtension implements ExpressionTypeResolver
 
         try {
             if ($value === null && $this->propertyDefaultValue) {
-                $value = $reflection->getNativeReflection()
-                    ->getProperty($this->propertyDefaultValue)
-                    ->getDefaultValue();
-
-                return is_string($value) ? $value : null;
+                $default = $reflection->getNativeReflection()
+                    ->getProperty('defaultTable')
+                    ->getDefaultValueExpression();
+                if ($default instanceof String_) {
+                    return $default->value;
+                }
             }
+
+            return null;
         } catch (ReflectionException) {
             return null;
         }
-
-        return null;
     }
 }
